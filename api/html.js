21690a86bc1f530dead189ef87a4c063a1e3b4cb
silentforest134531 <chino134531@gmail.com>
2026-08-16 19:04:59 +0800

    Update html.js

--- api/html.js
+++ api/html.js
@@ -1,16 +1,16 @@
 export default async function handler(req, res) {
   async function FetchData() {
-    const query = req.query.q
+    const query = encodeURIComponent(req.query.q)
     const token = process.env.GITHUB_TOKEN
     if (!token) {
       throw new Error('No token found')
     }
-    const url = `https://api.github.com/search/code?q=path:${query} OR content:${query}`;
+    const url = `https://api.github.com/search/code?q=path:${query}+OR+content:${query}`;
     try {
       const rsp = await fetch(url, {
         method: 'GET',
         headers: {
-          `Authorization': Bearer ${token}`,
+          'Authorization': `Bearer ${token}`,
           'Accept': 'application/vnd.github+json',
           'User-Agent': 'wweb (Github: silentforest134531)'
         }
