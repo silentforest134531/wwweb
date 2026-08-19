a3048d0a381bc2e77be4782a81fd5db9d6c0ac3c
silentforest134531 <chino134531@gmail.com>
2026-08-19 15:01:23 +0800

    Update html.mjs

--- api/html.mjs
+++ api/html.mjs
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         headers: {
           'Authorization': `Bearer ${token}`,
           'Accept': 'application/vnd.github+json',
-          'User-Agent': 'wweb (Github: silentforest134531)'
+          'User-Agent': 'wweb (Github: silentforest134531)',
           'X-GitHub-Api-Version': '2026-03-10'
         }
       });
