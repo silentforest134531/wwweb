3b3fe3475fd64dcbc1eded634660349c786cac61
silentforest134531 <chino134531@gmail.com>
2026-09-03 18:24:16 +0800

    Update html.mjs

--- api/html.mjs
+++ api/html.mjs
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+/* export default async function handler(req, res) {
   async function FetchData() {
     const query = encodeURIComponent(req.query.q)
     const token = process.env.GITHUB_TOKEN
